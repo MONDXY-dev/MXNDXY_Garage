1091b3e0e893790c35abaa038980a328436ad137
MONDXY-dev <oakmonday15@gmail.com>
2024-12-23 00:54:19 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerberus'
+game 'gta5'
+
+author 'MXNDXY-dev'
+description 'MXNDXY_Garage'
+version '1.0.0'
+
+shared_scripts {
+  'shared/cfg/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
